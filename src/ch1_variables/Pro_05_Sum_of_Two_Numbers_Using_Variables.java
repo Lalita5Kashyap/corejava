b0ed04141151ef6292c8c9e8b6b44ab433496a9a
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-03-23 11:09:03 +0530

    Modification in Packages in program Names

---
+++ src/ch1_variables/Pro_05_Sum_of_Two_Numbers_Using_Variables.java
@@ -0,0 +1,18 @@
+package ch1_variables;
+
+public class Pro_05_Sum_of_Two_Numbers_Using_Variables {
+
+	public static void main(String[] args) {
+		//Take two variables and sum their values.
+		
+				int a=3;
+				int b=8;
+				int sum;
+				//logic a+b
+				 sum=a+b;
+				 System.out.print("Sum of a+b: ");
+				 System.out.println(sum);
+				 
+	}
+
+}
---
+++ src/ch4_Decision_Statement/Pro_01_WAP_To_Check_Student_Pass_In_Examination.java
@@ -0,0 +1,17 @@
+package ch4_Decision_Statement;
+
+public class Pro_01_WAP_To_Check_Student_Pass_In_Examination {
+
+	public static void main(String[] args) {
+		int marks = 90;
+		int passingMarks = 35;
+
+		if (marks > passingMarks) {
+			System.out.println("Student is passed in the examination");
+		} else {
+			System.out.println("Student is failed in the examination");
+		}
+
+	}
+
+}
